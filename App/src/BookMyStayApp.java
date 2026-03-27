218b40745193ffa6c03913b8893675db94a2a490
MDVirinchi <rushi18126@gmail.com>
2026-03-27 15:06:05 +0530

    UC3

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -1,17 +1,28 @@
+import java.util.HashMap;
+
 public class BookMyStayApp {
     static void main() {
-                String[] roomTypes = {"Single Room", "Double Room", "Deluxe Room"};
-                int[] availability = {5, 3, 2};
+        String[] roomTypes = {"Single Room", "Double Room", "Deluxe Room"};
+        int[] availability = {5, 3, 2};
+        HashMap<String, Integer> roomInventory = new HashMap<>();
+        roomInventory.put("Single Room", 5);
+        roomInventory.put("Double Room", 3);
+        roomInventory.put("Deluxe Room", 2);
 
-                System.out.println("Welcome to the Hotel Booking Management System");
-                System.out.println("System initialized successfully.");
-                System.out.println("Welcome to the Hotel Booking Management System");
-                System.out.println("Available Room Types:\n");
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("Available Room Types:\n");
 
-                for (int i = 0; i < roomTypes.length; i++) {
-                    System.out.println(roomTypes[i] + " - Available: " + availability[i]);
-                }
-
-                System.out.println("\nSystem initialized successfully.");
-            }
+        for (int i = 0; i < roomTypes.length; i++) {
+            System.out.println(roomTypes[i] + " - Available: " + availability[i]);
         }
+
+        System.out.println("\nSystem initialized successfully.");
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("Room Inventory:\n");
+        for (String roomType : roomInventory.keySet()) {
+            System.out.println(roomType + " - Available: " + roomInventory.get(roomType));
+        }
+    }
+
+
+}
